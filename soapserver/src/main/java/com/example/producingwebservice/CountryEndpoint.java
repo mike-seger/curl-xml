96ab6cee4591efd8aee289d3d7538674b4799ca9
michael <michael@mac>
2021-07-10 22:45:30 +0200

    updated with list response

--- soapserver/src/main/java/com/example/producingwebservice/CountryEndpoint.java
+++ soapserver/src/main/java/com/example/producingwebservice/CountryEndpoint.java
@@ -1,5 +1,7 @@
 package com.example.producingwebservice;
 
+import io.spring.guides.gs_producing_web_service.GetCountriesRequest;
+import io.spring.guides.gs_producing_web_service.GetCountriesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -25,7 +27,14 @@ public class CountryEndpoint {
 	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
 		GetCountryResponse response = new GetCountryResponse();
 		response.setCountry(countryRepository.findCountry(request.getName()));
+		return response;
+	}
 
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountriesRequest")
+	@ResponsePayload
+	public GetCountriesResponse getCountries(@RequestPayload GetCountriesRequest request) {
+		GetCountriesResponse response = new GetCountriesResponse();
+		response.getCountriesList().addAll(countryRepository.findCountries(request.getNameList()));
 		return response;
 	}
 }
--- soapserver/src/main/java/com/example/producingwebservice/CountryRepository.java
+++ soapserver/src/main/java/com/example/producingwebservice/CountryRepository.java
@@ -1,8 +1,11 @@
 package com.example.producingwebservice;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import io.spring.guides.gs_producing_web_service.Country;
 import io.spring.guides.gs_producing_web_service.Currency;
@@ -15,33 +18,29 @@ public class CountryRepository {
 
 	@PostConstruct
 	public void initData() {
-		Country spain = new Country();
-		spain.setName("Spain");
-		spain.setCapital("Madrid");
-		spain.setCurrency(Currency.EUR);
-		spain.setPopulation(46704314);
+		addCountry("France", "Paris", Currency.EUR, 	65273511);
+		addCountry("Germany", "Berlin", Currency.EUR, 83783942);
+		addCountry("Italy", "Rome", Currency.EUR, 60461826);
+		addCountry("Switzerland", "Bern", Currency.CHF, 8654622);
+		addCountry("Spain", "Madrid", Currency.EUR, 46754778);
+		addCountry("United Kingdom", "London", Currency.EUR, 67886011);
+	}
 
-		countries.put(spain.getName(), spain);
-
-		Country poland = new Country();
-		poland.setName("Poland");
-		poland.setCapital("Warsaw");
-		poland.setCurrency(Currency.PLN);
-		poland.setPopulation(38186860);
-
-		countries.put(poland.getName(), poland);
-
-		Country uk = new Country();
-		uk.setName("United Kingdom");
-		uk.setCapital("London");
-		uk.setCurrency(Currency.GBP);
-		uk.setPopulation(63705000);
-
-		countries.put(uk.getName(), uk);
+	private void addCountry(String name, String capital, Currency currency, long population) {
+		Country country = new Country();
+		country.setName(name);
+		country.setCapital(capital);
+		country.setCurrency(currency);
+		country.setPopulation(population);
+		countries.put(country.getName(), country);
 	}
 
 	public Country findCountry(String name) {
 		Assert.notNull(name, "The country's name must not be null");
 		return countries.get(name);
+	}
+
+	public List<Country> findCountries(List<String> names) {
+		return countries.values().stream().filter(c -> names.contains(c.getName())).collect(Collectors.toList());
 	}
 }
