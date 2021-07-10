07c9db31082df2ba548e7469b13e4cf8bb4ffd5a
michael <gitlab@net128.com>
2021-07-10 10:19:47 +0200

    cleanup

--- src/main/java/curl/xml/App.java
+++ src/main/java/curl/xml/App.java
@@ -1,24 +1,16 @@
 package curl.xml;
 
-import net.sf.saxon.lib.Feature;
-import net.sf.saxon.lib.FeatureKeys;
 import net.sf.saxon.s9api.*;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 
-import javax.xml.XMLConstants;
-import javax.xml.transform.*;
-import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import net.sf.saxon.s9api.*;
-import net.sf.saxon.functions.*;
 
 import static org.toilelibre.libe.curl.Curl.curl;
 
@@ -58,44 +50,19 @@ public class App {
         }
     }
 
-    public String transform(InputStream xmlIn, InputStream xslIn) throws TransformerException, IOException, SaxonApiException {
-        /*try (InputStream xml = xmlIn)*/ {
-            /*try (InputStream xslt = xslIn)*/ {
+    public String transform(InputStream xmlIn, InputStream xslIn) throws IOException, SaxonApiException {
+        try (InputStream xml = xmlIn) {
+            try (InputStream xslt = xslIn) {
                 Processor processor = new Processor(false);
-                processor.setConfigurationProperty(net.sf.saxon.lib.Feature.SUPPRESS_XSLT_NAMESPACE_CHECK, false);
-                processor.setConfigurationProperty(net.sf.saxon.lib.Feature.ERROR_LISTENER_CLASS,
-                        NullErrorListener.class.getName());
-//                XsltCompiler comp = processor.newXsltCompiler();
-//                XsltExecutable exp = comp.compile(new StreamSource(xslIn));
-
-                Source xmlSource = new StreamSource(xmlIn);
+                Source xmlSource = new StreamSource(xml);
                 StringWriter sw = new StringWriter();
-                //Result result = new StreamResult(sw);
-
                 XsltCompiler compiler = processor.newXsltCompiler();
-                XsltExecutable stylesheet = compiler.compile(new StreamSource(xslIn));
+                XsltExecutable stylesheet = compiler.compile(new StreamSource(xslt));
                 Serializer out = processor.newSerializer(sw);
-
-                //out.setOutputProperty(Serializer.Property.METHOD, "text");
-                //out.setOutputProperty(Serializer.Property.INDENT, "no");
-//                TransformerFactory factory = TransformerFactory.newInstance();
-//                factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
-//                factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
-//                Transformer transformer = factory.newTransformer(new StreamSource(xslt));
-//                transformer.transform(xmlSource, out);
-                //XsltTransformer trans = exp.load();
                 Xslt30Transformer trans = stylesheet.load30();
-                //trans.setInitialContextNode(source);
-                //trans.setDestination(out);
                 trans.transform(xmlSource, out);
                 return sw.toString();
             }
         }
-    }
-
-    public class NullErrorListener implements ErrorListener {
-        @Override public void warning(TransformerException e) throws TransformerException { }
-        @Override public void error(TransformerException e) throws TransformerException { }
-        @Override public void fatalError(TransformerException e) throws TransformerException { }
     }
 }
