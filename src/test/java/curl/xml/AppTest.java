6b0dc47a63771b887487872c6b82e740f41fa73e
michael <michael@mac>
2021-07-12 10:52:32 +0200

    lib friendly

--- src/test/java/curl/xml/AppTest.java
+++ src/test/java/curl/xml/AppTest.java
@@ -20,10 +20,10 @@ public class AppTest implements TestAware {
     @Test
     public void testInputTransform() throws IOException {
         String result = new CurlXml().call(
-                null,
-                "name\nSpain\nFrance",
-                List.of("classpath:/soap-server/name-list-2-request.xsl"),
-                Collections.emptyList(), true);
+            null,
+            "name\nSpain\nFrance",
+            List.of("classpath:/soap-server/name-list-2-request.xsl"),
+            Collections.emptyList(), true);
         assertXmlMatches("/soap-server/request-ns-stripped.xml", result);
     }
 }
